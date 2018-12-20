700d31e9830f82f9d50adf4bddf50218f81d73c7
jhackett1 <joshua.hackett93@gmail.com>
2018-12-20 11:35:38 +0000

    Added animations to work page

--- src/js/__modules/animations.js
+++ src/js/__modules/animations.js
@@ -1,42 +1,29 @@
 import bodymovin from 'lottie-web';
 
+const initAnimation = (id, filename) => {
+    if(document.getElementById(id)){
+        bodymovin.loadAnimation({
+            container: document.getElementById(id),
+            path: `/assets/animations/${filename}`,
+            name: id,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true
+        })
+    }
+}
+
 const startAnimations = () => {
-    let creativeInsights = bodymovin.loadAnimation({
-        container: document.getElementById('creative-insights-animation'),
-        path: '/assets/animations/creativeinsights_animation.json',
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        name: "Creative insights",
-    });
-
-    let howTo = bodymovin.loadAnimation({
-        container: document.getElementById('how-to-animation'),
-        path: '/assets/animations/howto_animation.json',
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        name: "How to",
-    });
-
-    let sectorChallenges = bodymovin.loadAnimation({
-        container: document.getElementById('sector-challenges-animation'),
-        path: '/assets/animations/bullseye_animation.json',
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        name: "Sector challenges",
-    });
-
-    // let talk = bodymovin.loadAnimation({
-    //     container: document.getElementById('talk-animation'),
-    //     path: '/assets/animations/Talk_animation.json',
-    //     renderer: 'svg',
-    //     loop: true,
-    //     autoplay: true,
-    //     name: "Talk",
-    // });
-
+    // Insights page
+    initAnimation('creative-insights-animation', 'creative_insights_animation.json')
+    initAnimation('how-to-animation', 'how_to_animation.json')
+    initAnimation('sector-challenges-animation', 'bullseye_animation.json')
+    // Contact us icon
+    initAnimation('talk-animation', 'Talk_animation.json')
+    // Work page
+    initAnimation('brand-storytelling-animation', 'Eye_animation.json')
+    initAnimation('corporate-comms-animation', 'Lightbulb_animation.json')
+    initAnimation('educational-content-animation', 'Globe_animation.json')
 };
 
 export default startAnimations;
