c0a488f9842fd62906ff944058ce09a9107af73a
Joshua Hackett <joshua.hackett93@gmail.com>
2019-01-03 15:06:14 +0000

    Change lambda function endpoint

--- src/functions/get-events.js
+++ src/functions/get-events.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch"
 
 exports.handler = async (event, context, callback) => {
     // Get a soonest-first feed of a given organiser's events
-    const eventbriteFeed = `https://www.eventbriteapi.com/v3/events/search/?sort_by=date&organizer.id=${process.env.OID}&token=${process.env.EVENTBRITE_TOKEN}`
+    const eventbriteFeed = `https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&time_filter=current_future&token=${process.env.EVENTBRITE_TOKEN}`
     const response = await fetch(eventbriteFeed)
     const data = await response.json()
     return  {
