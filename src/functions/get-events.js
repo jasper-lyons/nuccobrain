33d623782a92117a5574fa9927c83426b1a70200
Joshua Hackett <joshua.hackett93@gmail.com>
2019-01-03 16:25:05 +0000

    Remove async/await

--- src/functions/get-events.js
+++ src/functions/get-events.js
@@ -1,19 +1,21 @@
-import "@babel/polyfill";
 import fetch from "node-fetch"
 
-exports.handler = async (event, context, callback) => {
+exports.handler = (event, context, callback) => {
+
   try {
     // Get a soonest-first feed of a given organiser's events
     const eventbriteFeed = `https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&time_filter=current_future&token=${process.env.EVENTBRITE_TOKEN}`
-    const response = await fetch(eventbriteFeed)
-    const data = await response.json()
-    return  {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify(data.events)
-    }
+    fetch(eventbriteFeed)
+      .then(res=>res.json())
+      .then(data=>{
+        return  {
+          statusCode: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*"
+          },
+          body: JSON.stringify(data.events)
+        }
+      })
   } catch(e) {
     console.log(e)
     return  {
@@ -24,4 +26,6 @@ exports.handler = async (event, context, callback) => {
       body: JSON.stringify(e)
     }
   }
+
+
 }
