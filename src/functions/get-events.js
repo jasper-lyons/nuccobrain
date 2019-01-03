67a00d255c0b2e0aff1b7a47cab0956e9ea6c17e
Joshua Hackett <joshua.hackett93@gmail.com>
2019-01-03 16:26:34 +0000

    remove try/catch

--- src/functions/get-events.js
+++ src/functions/get-events.js
@@ -2,7 +2,6 @@ import fetch from "node-fetch"
 
 exports.handler = (event, context, callback) => {
 
-  try {
     // Get a soonest-first feed of a given organiser's events
     const eventbriteFeed = `https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&time_filter=current_future&token=${process.env.EVENTBRITE_TOKEN}`
     fetch(eventbriteFeed)
@@ -16,16 +15,5 @@ exports.handler = (event, context, callback) => {
           body: JSON.stringify(data.events)
         }
       })
-  } catch(e) {
-    console.log(e)
-    return  {
-      statusCode: 400,
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      body: JSON.stringify(e)
-    }
-  }
-
 
 }
